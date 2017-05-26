86374520dc10e4b0ff80ce3f1e48effe7332f843
wango8311 <orson2000123456789@gmail.com>
2017-05-26 11:06:56 -0700

    Added location, fixed switchview

--- MyApplication/app/src/main/java/com/example/wango8311/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/wango8311/myapplication/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.wango8311.myapplication;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,9 +14,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-int a = 0;
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    int a = 0;
     private GoogleMap mMap;
 
     @Override
@@ -43,13 +48,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(van).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(van));
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return;
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},2);
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        LatLng here = new LatLng(49.2611813, -123.12541899999997);
-        mMap.addMarker(new MarkerOptions().position(here).title("You are here"));
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},2);
+        }
+        mMap.setMyLocationEnabled(true);
+
+
+
+        LatLng g = new LatLng(57.401219, 93.519662);
+        mMap.addMarker(new MarkerOptions().position(g).title("Welcome to Gulag, comrade"));
+
+
 
     }
-    public void switchView(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void switchView(View v) {
+
 
         if(a==0) {
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
